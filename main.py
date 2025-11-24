f5713127951fd27875bf8c1b536b2e02905909f3
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2025-11-24 16:09:44 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+#1-misol
+matn = input("text kirit: ")
+find = input("Qidirilayotganni kirit: ")
+
+if find not in matn:
+    print("topildi")
+else:
+    print("topilmadi")
+
+#2-misol
+matn = input("text kirit: ")
+find = input("Qidirilayotganni kirit: ")
+
+if find not in matn:
+    print("topildi")
+else:
+    print("topilmadi")
+
+#4-misol
+matn = input("text kirit: ")
+find = input("Qidirilayotganni kirit: ")
+
+if find not in matn:
+    print("topildi")
+else:
+    print("topilmadi")
+
+#5-misol
+matn = input("text kirit: ")
+find = input("Qidirilayotganni kirit: ")
+
+    if find not in matn:
+        print("topildi")
+    else:
+        print("topilmadi")
